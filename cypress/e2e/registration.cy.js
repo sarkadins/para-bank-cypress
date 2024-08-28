44ba267f33894b35efb8a43c4066d1a0a556b48b
sarkadins <soma.sarkadi@gmail.com>
2024-08-28 15:16:31 +0200

    add registration feature

---
+++ cypress/e2e/registration.cy.js
@@ -0,0 +1,18 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://parabank.parasoft.com/parabank/')
+    cy.get('#loginPanel > :nth-child(3) > a').click()
+    cy.get("input[id='customer.firstName']").type("John")
+    cy.get("input[id='customer.lastName']").type("Doe")
+    cy.get("input[id='customer.address.street']").type("18 Test street")
+    cy.get("input[id='customer.address.city']").type("Huntsville")
+    cy.get("input[id='customer.address.state']").type("Alabama")
+    cy.get("input[id='customer.address.zipCode']").type("3604")
+    cy.get("input[id='customer.phoneNumber']").type("12345678")
+    cy.get("input[id='customer.ssn']").type("6842396")
+    cy.get("input[id='customer.username']").type("john_doe")
+    cy.get("input[id='customer.password']").type("test1234")
+    cy.get("#repeatedPassword").type("test1234")
+    cy.get("input[value='Register']").click()    
+  })
+})
